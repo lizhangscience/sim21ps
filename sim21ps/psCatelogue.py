35759a425edee8d06135f773c3d28bfcf1c5be1d
Zhixian Ma <zxma_sjtu@qq.com>
2016-10-08 23:44:22 +0800

    psCatelogue.py: introduced healpy and rewrited

--- sim21ps/psCatelogue.py
+++ sim21ps/psCatelogue.py
@@ -21,13 +21,15 @@ References
     next generation radio telescopes[J]. Monthly Notices of the Royal
     Astronomical Society, 2008, 388(3):1335–1348.
 [2] Jelic et al.
-
+[3] Healpix, healpy.readthedocs.io
 """
 # Import packages or modules
 import os
+# import healpy as hp  # healpy
 import numpy as np
 import time
 from pandas import DataFrame
+import astropy.units as au
 # Custom module
 import basic_params
 
@@ -40,15 +42,15 @@ class PointSource:
     ----------
     z: float
         Redshift, z ~ U(0,20)
-    dA: float
+    dA: au.Mpc
         Angular diameter distance, which is calculated according to the cosmology
         constants. In this work, it is calculated by module basic_params
-    core_x,core_y: float
-        Core coordinates of the point source, which obey to uniform distribution.
-    Lumo: float
-        Brightness temperature, [mK] ?
-        area: float
-                Area of the point sources, pix^2
+    theta: au.rad
+        The colatitude angle in the spherical coordinate system
+    phi: au.rad
+        The longtitude angle in the spherical coordinate system
+    area: au.sr
+        Area of the point sources, sr = rad^2
 
     Functions
     ---------
@@ -58,29 +60,25 @@ class PointSource:
 
     """
     # Init
-    img_size = 0
     z = 0
-    dA = 0
-    core_x = 0
-    core_y = 0
-    area = 1
+    dA = 0 * au.Mpc * 0
+    theta = au.deg * 0
+    phi = au.deg * 0
+    area = au.sr * 0
     Columns = []
     nCols = 0
 
-    def __init__(self, img_size=512, ang_total=1):
-        # Image size
-        self.img_size = img_size
+    def __init__(self,nside=512):
         # Redshift
         self.z = np.random.uniform(0, 20)
         # angular diameter distance
-        self.ang_total = ang_total
-        self.Param = basic_params.PixelParams( self.z, self.img_size, self.ang_total)
+        self.Param = basic_params.PixelParams( self.z)
         self.dA = self.Param.dA
         # Area
-        self.area = 1
+        self.area = 4*np.pi/(12*nside**2)
         # PS_list information
-        self.Columns = ['z', 'dA (Mpc)', 'Core_x (pix)',
-                        'Core_y (pix)', 'Area']
+        self.Columns = ['z', 'dA (Mpc)', 'Theta (deg)',
+                        'Phi (deg)', 'Area (sr)']
         self.nCols = 5
 
     def gen_sgl_ps(self):
@@ -91,15 +89,14 @@ class PointSource:
         # Redshift
         self.z = np.random.uniform(0, 20)
         # angular diameter distance
-        self.Param = basic_params.PixelParams(
-            self.z, self.img_size, self.ang_total)
+        self.Param = basic_params.PixelParams(self.z)
         self.dA = self.Param.dA
         # Position
-        self.core_x = np.random.uniform(self.img_size - 1) + 1
-        self.core_y = np.random.uniform(self.img_size - 1) + 1
+        self.theta = np.random.uniform(0,np.pi) * 180 * au.deg
+        self.phi = np.random.uniform(0,np.pi*2) * 180 * au.deg
 
         PS_list = np.array(
-            [self.z, self.dA.value, self.core_x, self.core_y, self.area])
+            [self.z, self.dA.value, self.theta.value, self.phi.value, self.area.value])
         return PS_list
 
     def save_as_csv(self, NumPS=100, folder_name='PS_tables/'):
@@ -130,20 +127,20 @@ class StarForming(PointSource):
     Generate star forming point sources, inheritate from PointSource class.
     """
     # Init
-    radius = 0
+    radius = 0 * au.rad
     Lumo_1400 = 0
 
-    def __init__(self, Lumo_1400=1500, img_size=512, ang_total=1):
-        PointSource.__init__(self, img_size, ang_total)
+    def __init__(self, nside = 512,Lumo_1400=1500):
+        PointSource.__init__(self, nside)
         self.Lumo_1400 = Lumo_1400
-        self.Columns.append('radius')
+        self.Columns.append('radius (rad)')
         self.nCols += 1
 
     def get_radius(self):
         Temp = 0.22 * np.log10(self.Lumo_1400) - np.log10(1 + self.z) - 3.32
-        self.radius = 10 ** Temp / 2
+        self.radius = 10 ** Temp / 2 * au.Mpc
 
-        return self.scale
+        return self.radius
 
     def gen_sgl_ps(self):
         """
@@ -153,18 +150,17 @@ class StarForming(PointSource):
         # Redshift
         self.z = np.random.uniform(0, 20)
         # angular diameter distance
-        self.Param = basic_params.PixelParams(
-        self.z, self.img_size, self.ang_total)
+        self.Param = basic_params.PixelParams(self.z)
         self.dA = self.Param.dA
-        self.radius = self.Param.get_angle(self.get_radius())[0]
+        self.radius = self.Param.get_angle(self.get_radius()) # [rad]
         # Area
-        self.area = np.pi * self.radius**2
+        self.area = np.pi * self.radius**2  #[sr] ?
         # Position
-        self.core_x = np.random.uniform(self.img_size - 1) + 1
-        self.core_y = np.random.uniform(self.img_size - 1) + 1
+        self.theta = np.random.uniform(0,np.pi) * 180 * au.deg
+        self.phi = np.random.uniform(0,np.pi*2) * 180 * au.deg
 
         PS_list = np.array(
-            [self.z, self.dA.value, self.core_x, self.core_y, self.area, self.radius])
+            [self.z, self.dA.value, self.theta.value, self.phi.value, self.area.value, self.radius.value])
         return PS_list
 
     def save_as_csv(self, NumPS=100, folder_name='PS_tables'):
@@ -197,9 +193,9 @@ class StarBursting(PointSource):
     # Init
     radius = 0
 
-    def __init__(self, img_size=512, ang_total=1):
-        PointSource.__init__(self, img_size, ang_total)
-        self.Columns.append('radius')
+    def __init__(self,nside=512):
+        PointSource.__init__(self,nside)
+        self.Columns.append('radius (rad)')
         self.nCols += 1
 
     def get_radius(self):
@@ -218,18 +214,17 @@ class StarBursting(PointSource):
         # Redshift
         self.z = np.random.uniform(0, 20)
         # angular diameter distance
-        self.Param = basic_params.PixelParams(
-            self.z, self.img_size, self.ang_total)
+        self.Param = basic_params.PixelParams(self.z)
         self.dA = self.Param.dA
-        self.radius = self.Param.get_angle(self.get_radius())[0]
+        self.radius = self.Param.get_angle(self.get_radius())
         # Area
-        self.area = np.pi * self.radius**2
+        self.area = np.pi * self.radius**2 #[sr]
         # Position
-        self.core_x = np.random.uniform(self.img_size - 1) + 1
-        self.core_y = np.random.uniform(self.img_size - 1) + 1
+        self.theta = np.random.uniform(0,np.pi) * 180 * au.deg
+        self.phi = np.random.uniform(0,np.pi*2) * 180 * au.deg
 
         PS_list = np.array(
-            [self.z, self.dA.value, self.core_x, self.core_y, self.area, self.radius])
+            [self.z, self.dA.value, self.theta.value, self.phi.value, self.area.value, self.radius.value])
         return PS_list
 
     def save_as_csv(self, NumPS=100, folder_name='PS_tables'):
@@ -271,29 +266,29 @@ class FRI(PointSource):
 
     """
     # New parameters
-    lobe_maj = 0
-    lobe_min = 0
-    lobe_ang = 0
+    lobe_maj = 0 * au.rad
+    lobe_min = 0 * au.rad
+    lobe_ang = 0 * au.deg
 
-    def __init__(self):
-        PointSource.__init__(self, img_size=512, ang_total=1)
+    def __init__(self,nside=512):
+        PointSource.__init__(self, nside)
         self.Columns.extend(
-            ['lobe_maj', 'lobe_min', 'lobe_ang'])
+            ['lobe_maj (rad)', 'lobe_min (rad)', 'lobe_ang (deg)'])
         self.nCols += 3
 
     def gen_lobe(self):
         """
         According to Wang's work, the linear scale at redshift z obeys to U(0,D0(1+z)^(-1.4))
         """
-        D0 = 1
-        self.lobe_maj = 0.5 * np.random.uniform(0, D0 * (1 + self.z)**(-1.4))
-        self.lobe_min = self.lobe_maj * np.random.uniform(0.2, 1)
-        self.lobe_ang = np.random.uniform(0, np.pi)
+        D0 = 1 * au.Mpc
+        self.lobe_maj = 0.5 * np.random.uniform(0, D0.value * (1 + self.z)**(-1.4)) * au.Mpc
+        self.lobe_min = self.lobe_maj * np.random.uniform(0.2, 1) * au.Mpc
+        self.lobe_ang = np.random.uniform(0, np.pi) * 180 * au.deg
 
         # Transform to pixel
-        self.lobe_maj = self.Param.get_angle(self.lobe_maj)[0]
-        self.lobe_min = self.Param.get_angle(self.lobe_min)[0]
-        lobe = [self.lobe_maj, self.lobe_min, self.lobe_ang]
+        self.lobe_maj = self.Param.get_angle(self.lobe_maj)
+        self.lobe_min = self.Param.get_angle(self.lobe_min)
+        lobe = [self.lobe_maj.value, self.lobe_min.value, self.lobe_ang.value]
 
         return lobe
 
@@ -305,12 +300,12 @@ class FRI(PointSource):
         # Redshift
         self.z = np.random.uniform(0, 20)
         # angular diameter distance
-        self.Param = basic_params.PixelParams(self.z, self.img_size, self.ang_total)
+        self.Param = basic_params.PixelParams(self.z)
         self.dA = self.Param.dA
 
         # Position
-        self.core_x = np.random.uniform(self.img_size - 1) + 1
-        self.core_y = np.random.uniform(self.img_size - 1) + 1
+        self.theta = np.random.uniform(0,np.pi) * 180 * au.deg
+        self.phi = np.random.uniform(0,np.pi*2) * 180 * au.deg
 
         # Area
         self.area = np.pi * self.lobe_maj * self.lobe_min
@@ -318,7 +313,7 @@ class FRI(PointSource):
         # lobe
         lobe = self.gen_lobe()
 
-        PS_list = [self.z, self.dA.value, self.core_x, self.core_y, self.area]
+        PS_list = [self.z, self.dA.value, self.theta.value, self.phi.value, self.area.value]
         PS_list.extend(lobe)
 
         PS_list = np.array(PS_list)
@@ -353,21 +348,21 @@ class FRII(FRI):
     Generate Faranoff-Riley I (FRI) AGN, a class inherit from FRI
     """
 
-    def __init__(self,img_size = 512,ang_total = 1):
-        FRI.__init__(self,img_size,ang_total)
+    def __init__(self,nside = 512):
+        FRI.__init__(self,nside)
 
     def gen_lobe(self):
         """
         According to Wang's work, the linear scale at redshift z obeys to U(0,D0(1+z)^(-1.4))
         """
         D0 = 1
-        self.lobe_maj = 0.5 * np.random.uniform(0, D0 * (1 + self.z)**(-1.4))
-        self.lobe_min = self.lobe_maj * np.random.uniform(0.2, 1)
-        self.lobe_ang = np.random.uniform(0, np.pi / 3)  # Different from FRI
+        self.lobe_maj = 0.5 * np.random.uniform(0, D0 * (1 + self.z)**(-1.4)) * au.Mpc
+        self.lobe_min = self.lobe_maj * np.random.uniform(0.2, 1) * au.Mpc
+        self.lobe_ang = np.random.uniform(0, np.pi / 3) * 180 * au.deg # Different from FRI
 
         # Transform to pixel
-        self.lobe_maj = self.Param.get_angle(self.lobe_maj)[0]
-        self.lobe_min = self.Param.get_angle(self.lobe_min)[0]
-        lobe = [self.lobe_maj, self.lobe_min, self.lobe_ang]
+        self.lobe_maj = self.Param.get_angle(self.lobe_maj)
+        self.lobe_min = self.Param.get_angle(self.lobe_min)
+        lobe = [self.lobe_maj.value, self.lobe_min.value, self.lobe_ang.value]
 
         return lobe
